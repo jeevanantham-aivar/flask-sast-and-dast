bafacf5d65d12fc6268460d8cb4601b4edf1121c
jeevanantham-aivar <jeevanantham.n@aivar.tech>
2025-07-29 17:12:13 +0530

    trying to cover dast

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "   Hello, ECS from Flask testing testing! version 4"
+    return "   Hello, ECS from Flask testing version 5"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
